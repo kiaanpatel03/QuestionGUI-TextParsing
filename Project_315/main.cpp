6812536158b59841b0d92eb463b07d272a023775
JoshGoodwin10 <61861481+JoshGoodwin10@users.noreply.github.com>
2024-04-30 21:14:03 +0200

    Update main.cpp

--- Project_315/main.cpp
+++ Project_315/main.cpp
@@ -5,86 +5,79 @@
 #include <cstring>
 #include <random>
 #include <algorithm>
+#include <Question.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-struct Question
+// returns Question at specified index in the question bank
+Question getQuestion(int index, vector<string> questions)
 {
-    string questionID;
-    string questionType;
-    string questionDiff;
-    string question;
-    string questionPossibleAnswersString;
-    string questionAnswer;
+    Question returnQuestion;
 
-    void parseDetails(const string& line)
+    if (index > questions.capacity() || index < 0)
     {
-        vector<string> questionDetails;
-
-        // Copy the string to a non-const char array
-        char charLine[line.size() + 1];
-        strcpy(charLine, line.c_str());
-
-        char* ptr;
-        ptr = strtok(charLine, "#"); // Delimiters are #
-
-        // use while loop to check ptr is not null
-        while (ptr != NULL)
-        {
-            questionDetails.push_back(ptr); // push the string token
-            ptr = strtok (NULL, "#");
-        }
-
-        questionID = questionDetails[0];
-        questionType = questionDetails[1];
-        questionDiff = questionDetails[2];
-        question = questionDetails[3];
-        questionPossibleAnswersString = questionDetails[4];
-        questionAnswer = questionDetails[5];
+        cout << "Invalid index to fetch question from" << endl;
     }
 
-    void printDetails()
+    else
     {
-        cout << "Question ID: " << questionID << endl;
-        cout << "Question Type: " << questionType << endl;
-        cout << "Question Difficulty: " << questionDiff << endl;
-        cout << "Question: " << question << endl;
-        cout << "Possible Answers: " << endl;
-
-        vector<string> answers = parsePossibleAnswers();
-
-        for(const auto& answer : answers)
-        {
-            cout << "\t" << answer << endl;
-        }
-
-        cout << "Answer: " << questionAnswer << endl;
+        string line = questions[index];
+        returnQuestion.parseDetails(line);
     }
 
-    vector<string> parsePossibleAnswers()
+    return returnQuestion;
+}
+
+vector<Question> getQuestionsOfDifficulty(int numQuestions, string difficulty, vector<string> questions)
+{
+    vector<Question> returnQuestions(0);
+    vector<int> usedQuestions(0);    // store the question ids of questions already used
+
+    returnQuestions.reserve(numQuestions);
+    usedQuestions.reserve(numQuestions);
+
+    while(returnQuestions.size() < returnQuestions.capacity())
     {
-        vector<string> answers;
+        // Generate a random number
+        int randomNumber = rand() % 118;
+        //cout << randomNumber << endl;
 
-        // Copy the string to a non-const char array
-        char questionLine[questionPossibleAnswersString.size() + 1];
-        strcpy(questionLine, questionPossibleAnswersString.c_str());
-
-        char* Qptr;
-        Qptr = strtok(questionLine, "$"); // Delimiters are $
-
-        // use while loop to check ptr is not null
-        while (Qptr != NULL)
+        if (find(usedQuestions.begin(), usedQuestions.end(), randomNumber) != usedQuestions.end())
         {
-            answers.push_back(Qptr); // push the string token
-            Qptr = strtok (NULL, "$");
+            // go to next loop iteration
+            continue;
+        }
+        else
+        {
+            Question fetchedQuestion = getQuestion(randomNumber, questions);
+
+            if (fetchedQuestion.getQuestionDiff() == difficulty)
+            {
+                usedQuestions.push_back(randomNumber);
+                returnQuestions.push_back(fetchedQuestion);
+            }
+
+            else
+            {
+                continue;
+            }
         }
 
-        return answers;
     }
-};
+
+
+
+    return returnQuestions;
+}
+
+
 
 int main()
 {
+    srand(time(NULL));
+
     // read in the text file
     ifstream inputFile("C:/Users/Josh Goodwin/Desktop/School/UKZN/Third Year/COMP315/315 Projects/Project_315/WorldWar2Questions.txt");
     // variable to store each line of the text file as it is read in
@@ -108,14 +101,53 @@ int main()
     // Close the input file
     inputFile.close();
 
-    for (auto& line : lines)
-    {
-        cout << "\n" << line << "\n" << endl;
+    // Vectors to store 10 Questions of each difficulty
+    vector<Question> easyQs;
+    vector<Question> mediumQs;
+    vector<Question> hardQs;
 
-        Question question;
-        question.parseDetails(line);
-        question.printDetails();
-    }
+    cout << "\n" << easyQs.size() << "\n" << mediumQs.size()<< "\n" << hardQs.size() << endl;
+
+    easyQs = getQuestionsOfDifficulty(5, "E", lines);
+
+    cout << "\nQuestion1:\n" << endl;
+    easyQs[0].printDetails();
+    cout << "\nQuestion2:\n" << endl;
+    easyQs[1].printDetails();
+    cout << "\nQuestion3:\n" << endl;
+    easyQs[2].printDetails();
+    cout << "\nQuestion4:\n" << endl;
+    easyQs[3].printDetails();
+    cout << "\nQuestion5:\n" << endl;
+    easyQs[4].printDetails();
+
+    mediumQs = getQuestionsOfDifficulty(5, "M", lines);
+
+    cout << "Question1:\n" << endl;
+    mediumQs[0].printDetails();
+    cout << "\nQuestion2:\n" << endl;
+    mediumQs[1].printDetails();
+    cout << "\nQuestion3:\n" << endl;
+    mediumQs[2].printDetails();
+    cout << "\nQuestion4:\n" << endl;
+    mediumQs[3].printDetails();
+    cout << "\nQuestion5:\n" << endl;
+    mediumQs[4].printDetails();
+
+    hardQs = getQuestionsOfDifficulty(5, "H", lines);
+
+    cout << "Question1:\n" << endl;
+    hardQs[0].printDetails();
+    cout << "\nQuestion2:\n" << endl;
+    hardQs[1].printDetails();
+    cout << "\nQuestion3:\n" << endl;
+    hardQs[2].printDetails();
+    cout << "\nQuestion4:\n" << endl;
+    hardQs[3].printDetails();
+    cout << "\nQuestion5:\n" << endl;
+    hardQs[4].printDetails();
+
+    cout << "\n" << easyQs.size() << "\n" << mediumQs.size()<< "\n" << hardQs.size() << endl;
 
     return 0;
 }
